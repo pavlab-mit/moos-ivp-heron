67e4de9a607300b77cb92ce61adf9179dcff39fd
mikerb <mikerb@mit.edu>
2025-03-30 13:08:45 -0400

    initil setup

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build/*
+rm -rf lib/*
+rm -rf bin/*
+
+find . -name '.DS_Store'  -print -exec rm -rfv {} \;
+find . -name '*~'  -print -exec rm -rfv {} \;
+find . -name '#*'  -print -exec rm -rfv {} \;
+find . -name '*.moos++'  -print -exec rm -rfv {} \;
+
